5e0812dbefecdbbb86da9c4d5bacd4e0f51ca947
Muggle Potter <sanjanatripathi007@gmail.com>
2018-08-21 21:29:56 -0500

    modified contact page; added link to LIRI

---
+++ script.js
@@ -0,0 +1,9 @@
+
+            $(document).ready(function () {
+                $('.parallax').parallax();
+            });
+            $(document).ready(function () {
+                $('.fixed-action-btn').floatingActionButton();
+            });
+
+       
